3cd37fdfcf95f58b912003f2921d058c995ff512
hugo2233 <69150726+hugo1120@users.noreply.github.com>
2024-10-29 19:26:50 +0800

    Update workflow_download.py

--- usage/workflow_download.py
+++ usage/workflow_download.py
@@ -3,7 +3,18 @@ from jmcomic.cl import JmcomicUI
 
 # 下方填入你要下载的本子的id，一行一个，每行的首尾可以有空白字符
 jm_albums = '''
-JM474858
+JM554945
+JM402846
+JM401950
+JM14303
+JM220709
+JM218864
+JM48600
+JM6495
+JM6493
+JM6486
+
+
 '''
 
 # 单独下载章节
